a1795fe0c3f2772f0914c7b973bbf2c0146ff184
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 12:41:28 +0300

    Created an index file for the Tab group file

---
+++ app/(tab)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TabIndexPage = () => {
+	return (
+		<View>
+			<Text>TabIndexPage</Text>
+		</View>
+	)
+}
+
+export default TabIndexPage
