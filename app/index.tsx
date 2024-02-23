2710b4f5112025923ef3456d652a9a09bdcb786d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 12:12:12 +0300

    Centered all the content of the first page

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from 'react-native'
 
 const IndexPage = () => {
 	return (
-		<View>
+		<View
+			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+		>
 			<Text>IndexPage</Text>
 		</View>
 	)
