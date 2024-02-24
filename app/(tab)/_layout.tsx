6db70e2be4051563312389d04d1b40b8463f8464
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-24 08:22:27 +0300

    Formatted the layout file

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -1,19 +1,44 @@
-import { FontAwesome } from '@expo/vector-icons'
+import { FontAwesome, FontAwesome5 } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 import React from 'react'
 
 const TabsLayout = () => {
 	return (
 		<Tabs>
-            <Tabs.Screen name='home' options={{tabBarLabel: 'List', headerTitle: 'Home Screen', tabBarIcon: ({color, size}) => <FontAwesome name="list" size={size} color={color} />}} />
-            <Tabs.Screen
-                name='profile'
-                options={{
-                    tabBarLabel: 'Account',
-                    headerTitle: 'My Account',
-                    tabBarIcon: ({color, size}) => <FontAwesome name='user' size={size} color={color} />
-                }}
-            />
+			<Tabs.Screen
+				name='home'
+				options={{
+					tabBarLabel: 'List',
+					headerTitle: 'Home Screen',
+					tabBarIcon: ({ color, size }) => (
+						<FontAwesome name='list' size={size} color={color} />
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name='profile'
+				options={{
+					tabBarLabel: 'Account',
+					headerTitle: 'My Account',
+					tabBarIcon: ({ color, size }) => (
+						<FontAwesome name='user' size={size} color={color} />
+					)
+				}}
+			/>
+			<Tabs.Screen
+				name='list'
+				options={{
+					tabBarLabel: 'News',
+					headerShown: false,
+					tabBarIcon: ({ color, size }) => (
+						<FontAwesome5
+							name='newspaper'
+							size={size}
+							color={color}
+						/>
+					)
+				}}
+			/>
 		</Tabs>
 	)
 }
